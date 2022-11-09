5f5a2d3c1d3c5d13088e0f7f419174c66fc67c52
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2022-11-09 17:43:01 -0300

    mq celular teste

---
+++ js/cel.js
@@ -0,0 +1,39 @@
+
+document.addEventListener('TouchEvent', jump);
+let pontos = 0
+
+
+function jump() {
+    pontos += 100
+    const mario = document.getElementById('mario') 
+    mario.classList.add("jump")
+
+    let score = document.getElementById('pontos')
+    score.innerHTML = pontos
+
+    setTimeout(() => {
+        mario.classList.remove("jump")
+    }, 900);
+}
+
+const loop = setInterval(() => {
+    const pipe = document.querySelector('.pipe')
+    const mario = document.getElementById('mario') 
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+
+    if (pipePosition <= 70 && pipePosition > 0 && marioPosition < 60) {
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosition}px`
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = 'imagens/game-over.gif'
+
+        clearInterval(loop)
+    }
+    
+    
+} , 10)
