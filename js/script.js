acc1dbbb0400676b03f306adf6ee24fe9cd94330
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2022-11-09 15:22:25 -0300

    Terminado projeto

--- js/script.js
+++ js/script.js
@@ -1,29 +1,39 @@
 
 document.addEventListener('keydown', jump);
+let pontos = 0
+
 
 function jump() {
-    const mar = document.getElementById('mario') 
-    mar.classList.add("jump")
+    pontos += 100
+    const mario = document.getElementById('mario') 
+    mario.classList.add("jump")
+
+    let score = document.getElementById('pontos')
+    score.innerHTML = pontos
 
     setTimeout(() => {
-        mar.classList.remove("jump")
+        mario.classList.remove("jump")
     }, 900);
 }
 
 const loop = setInterval(() => {
     const pipe = document.querySelector('.pipe')
-    const mar = document.getElementById('mario') 
+    const mario = document.getElementById('mario') 
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mar).bottom.replace('px', '');
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
 
-    if (pipePosition <= 100 && pipePosition > 0 && marioPosition < 80) {
+    if (pipePosition <= 100 && pipePosition > 0 && marioPosition < 70) {
         pipe.style.animation = 'none'
         pipe.style.left = `${pipePosition}px`
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = 'imagens/game-over.gif'
+
+        clearInterval(loop)
     }
     
     
 } , 10)
-
-let score = document.querySelector('#score')
-score.innerHTML = `SCORE: ${c}`
